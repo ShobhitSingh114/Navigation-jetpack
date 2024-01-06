7d0b53331dded4da0d81acb2c1566f2324f33f6b
Shobhit <shobhitsingh1149201@gmail.com>
2024-01-07 00:39:11 +0530

    Experimental 3rd screen added

--- app/src/main/java/com/example/navigation_jetpack/Navigation.kt
+++ app/src/main/java/com/example/navigation_jetpack/Navigation.kt
@@ -1,6 +1,8 @@
 package com.example.navigation_jetpack
 
 import android.app.Activity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,7 +11,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -19,7 +25,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavType
@@ -55,6 +66,11 @@ fun Navigation() {
                 text = it.arguments?.getString("name"),
                 navController = navController
             )
+        }
+
+        // Screen 3
+        composable("pokemon_list_screen"){
+            PokemonListScreen(navController = navController)
         }
 
     }
@@ -138,7 +154,102 @@ fun DetailScreen(text: String?, navController: NavController) {
             }) {
                 Text("Exit")
             }
+            Button(
+                onClick = {
+                    navController.navigate("pokemon_list_screen")
+                }
+            ) {
+                Text(text = "Next")
+            }
         }
     }
 
+}
+
+
+@Composable
+fun PokemonListScreen(
+    navController: NavController
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column {
+            Spacer(modifier = Modifier.height(20.dp))
+            Image(
+                painter = painterResource(id = androidx.core.R.drawable.ic_call_answer),
+                contentDescription = "Pokemon",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+            )
+            SearchBar(
+                hint = "Search...",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            Button(onClick = {
+                navController.popBackStack()
+            }) {
+                Text("Back")
+            }
+            val activity = (LocalContext.current as? Activity)
+            Button(onClick = {
+                activity?.finish()
+            }) {
+                Text("Exit")
+            }
+        }
+    }
+}
+
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    hint: String = "",
+    onSearch: (String) -> Unit = {}
+) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    var isHintDisplayed by remember {
+        // if hint is not equal to empty string
+        // so if we pass something for the hint this will start with true
+        // and if we pass empty string here then this will be false
+        mutableStateOf( hint  != "")
+    }
+
+    Box(modifier = modifier) {
+        BasicTextField(
+            value = text,
+            onValueChange = {
+                text = it
+                onSearch(it)
+            },
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(5.dp, CircleShape)
+                .background(Color.White, CircleShape)
+                .padding(vertical = 12.dp, horizontal = 20.dp)
+                // it will get called when we clicked into that textfield bcz then we want to hind our hint
+                .onFocusChanged {
+                    // hint tab show hona jab BTF ka focusState false hona
+//                    isHintDisplayed = it != FocusState.Active
+                    isHintDisplayed = !it.isFocused
+                }
+        )
+        if (isHintDisplayed) {
+            Text(
+                text = hint,
+                color = Color.LightGray,
+                modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp)
+            )
+        }
+    }
 }
